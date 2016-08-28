dd5ae2987ec5606c021f4cbf2023a1f93aaee06c
spikespiegel5112 <583288277@qq.com>
2016-08-29 00:41:59 +0800

    temp

--- javaTest/src/HelloWorld.java
+++ javaTest/src/HelloWorld.java
@@ -2,13 +2,15 @@ import java.applet.*;
 import java.awt.*;
 
 public class HelloWorld {
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.blue);
+        g.drawString("aaaaaaaaaaaaaaaaaaaaaaaaaaa",30,20);
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World"); // 打印 Hello World
         HelloWorld hw=new HelloWorld();
         hw.paint();
-    }
-    public void paint(Graphics g){
-        g.setColor(Color.blue);
-        g.drawString("aaaaaaaaaaaaaaaaaaaaaaaaaaa",30,20);
     }
 }
