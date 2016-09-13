7799cd9c25b4a1861f8901e2eaefe1091fe0b8bf
wangbolun <583288277@qq.com>
2016-09-13 18:09:47 +0800

    temp

--- javaTest/src/HelloWorld.java
+++ javaTest/src/HelloWorld.java
@@ -1,32 +1,29 @@
 import java.applet.*;
 import java.awt.*;
-//import java.io.*;
-//import java.swing.*;
+import java.io.*;
+import javax.swing.*;
 
-class Employee{
-    String name,food;
-    void getName(String name){
-        this.name=name;
+class Employee {
+    String name, food;
+
+    void getName(String name) {
+        this.name = name;
     }
-    String printName(){
+
+    String printName() {
         return name;
     }
-    void getFood(String paraFood){
-        food=paraFood;
+
+    void getFood(String paraFood) {
+        food = paraFood;
     }
 }
 
 public class HelloWorld {
-    public void paint(Graphics g) {
-        Color c = g.getColor();
-        g.setColor(Color.blue);
-        g.drawString("aaaaaaaaaaaaaaaaaaaaaaaaaaa",30,20);
-    }
-
     public static void main(String args[]) {
-        HelloWorld hw=new HelloWorld();
-        Employee employee_1=new Employee();
-        String myName="spike";
+        HelloWorld hw = new HelloWorld();
+        Employee employee_1 = new Employee();
+        String myName = "spike";
         employee_1.getName(myName);
         employee_1.getFood("egg");
         System.out.println("Hello World"); // 打印 Hello World
@@ -35,5 +32,11 @@ public class HelloWorld {
         employee_1.getName("ccc");
         System.out.println(employee_1.printName());
         //hw.paint();
+    }
+
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.blue);
+        g.drawString("aaaaaaaaaaaaaaaaaaaaaaaaaaa", 30, 20);
     }
 }
